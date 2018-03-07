137796e50c0b00ebb85647d819948e66e21964e2
Noel Delgado <noeldelgadom@gmail.com>
2018-03-07 16:04:15 -0600

    25. Passing Data between Pages

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'page-user',
   templateUrl: 'user.html',
 })
-export class UserPage {
+export class UserPage implements OnInit {
   name: string;
+
+  constructor(
+    private navParams: NavParams
+  ) {}
+
+  ngOnInit() {
+    this.name = this.navParams.get('userName');
+  }
 }
--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,3 +1,4 @@
+import { UserPage } from './user/user';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +16,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UsersPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController
+  ) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UsersPage');
+  onLoadUser(name: string) {
+    this.navCtrl.push(UserPage, {userName: name});
   }
-
 }
